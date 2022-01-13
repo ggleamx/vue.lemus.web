8e2c043f8914d4b27a62a3f16b8029eaeca6c6ed
Moseph <joseph@gleam.mx>
2022-01-13 09:25:39 -0800

    basic skeleton: babel, router, custom '.browserlistrc' configs, custom zopfli compression, normalize.css and 'http-server' package + 'npm start' script to preview static build files locally

--- README.md
+++ README.md
@@ -1,19 +1,29 @@
 # lemus-enero
 
 ## Project setup
+
 ```
 npm install
 ```
 
 ### Compiles and hot-reloads for development
+
 ```
 npm run serve
 ```
 
 ### Compiles and minifies for production
+
 ```
 npm run build
 ```
 
+### Serves production files to preview locally
+
+```
+npm run start
+```
+
 ### Customize configuration
+
 See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ vue.config.js
@@ -0,0 +1,16 @@
+const zopfli = require("@gfx/zopfli");
+module.exports = {
+  pluginOptions: {
+    compression: {
+      zopfli: {
+        compressionOptions: {
+          numiterations: 15,
+        },
+        include: /\.(js|css|html|svg|json)(\?.*)?$/i,
+        algorithm(input, compressionOptions, callback) {
+          return zopfli.gzip(input, compressionOptions, callback);
+        },
+      },
+    },
+  },
+};
