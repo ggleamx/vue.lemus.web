4c7a034ab111fb960ce1e804cdf091b8f1e5e252
Joseph Medina <josephmedina599@gmail.com>
2022-10-13 20:28:16 -0700

    changes on price filter menu

--- src/components/SearchFilterBar/filters/PriceFilter.vue
+++ src/components/SearchFilterBar/filters/PriceFilter.vue
@@ -1,49 +1,54 @@
 <template>
-<div class="price-filter">
-    <div @click="toggle(this.position)" class="custom-filter">
-        <div class="cf-title">
-            <span>{{'PRECIO'}}</span>
+    <div class="price-filter">
+        <div @click="toggle(this.position)" class="custom-filter">
+            <div class="cf-title">
+                <span>{{'PRECIO'}}</span>
+            </div>
+
+            <div class="cf-icon">
+                <svg width="8" height="15" viewBox="0 0 20 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path
+                        d="M9.17782 14.569C9.26956 14.7022 9.39231 14.8112 9.53551 14.8864C9.6787 14.9617 9.83805 15.001 9.99981 15.001C10.1616 15.001 10.3209 14.9617 10.4641 14.8864C10.6073 14.8112 10.7301 14.7022 10.8218 14.569L19.8218 1.569C19.926 1.41906 19.9871 1.24343 19.9984 1.06121C20.0098 0.87898 19.971 0.697123 19.8863 0.535394C19.8016 0.373665 19.6741 0.238248 19.5179 0.143858C19.3616 0.049468 19.1824 -0.000286344 18.9998 1.23965e-06H0.999815C0.817658 0.000753649 0.639153 0.0511479 0.483495 0.145764C0.327838 0.24038 0.200917 0.375639 0.116383 0.536994C0.0318493 0.698349 -0.00710004 0.879696 0.00372392 1.06153C0.0145479 1.24337 0.0747356 1.41881 0.177815 1.569L9.17782 14.569Z"
+                        fill="black" />
+                </svg>
+            </div>
         </div>
 
-        <div class="cf-icon">
-            <svg width="8" height="15" viewBox="0 0 20 15" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M9.17782 14.569C9.26956 14.7022 9.39231 14.8112 9.53551 14.8864C9.6787 14.9617 9.83805 15.001 9.99981 15.001C10.1616 15.001 10.3209 14.9617 10.4641 14.8864C10.6073 14.8112 10.7301 14.7022 10.8218 14.569L19.8218 1.569C19.926 1.41906 19.9871 1.24343 19.9984 1.06121C20.0098 0.87898 19.971 0.697123 19.8863 0.535394C19.8016 0.373665 19.6741 0.238248 19.5179 0.143858C19.3616 0.049468 19.1824 -0.000286344 18.9998 1.23965e-06H0.999815C0.817658 0.000753649 0.639153 0.0511479 0.483495 0.145764C0.327838 0.24038 0.200917 0.375639 0.116383 0.536994C0.0318493 0.698349 -0.00710004 0.879696 0.00372392 1.06153C0.0145479 1.24337 0.0747356 1.41881 0.177815 1.569L9.17782 14.569Z" fill="black" />
-            </svg>
+        <div v-if="filterStatus(2)" class="price-dialog-filter">
+            <div class="dialog-filter-head">
+
+                <div class="dialog-filter-head-column min">
+                    precio min. {{min}}</div>
+                <div class="dialog-filter-head-column">precio max. {{max}}</div>
+
+            </div>
+            <div class="dialog-filter-head-column-options">
+
+                <div class="dialog-filter-head-option">
+                    cualquier precio</div>
+                <div class="dialog-filter-head-option">cualquier precio</div>
+
+            </div>
+            <!-- ****v-for ↓*** -->
+            <div class="dialog-filter-head-column-options prices">
+                <div class="dialog-filter-head-option" v-for="(range ,index) in minRanges" :key="index">
+                    {{ nFormatter(range,4)}}
+                </div>
+
+
+                <div class="dialog-filter-head-option" v-for="(range ,index) in maxRanges" :key="index" c>
+
+                    {{nFormatter(range,4)}}
+
+                </div>
+            </div>
+
+
+
+
         </div>
+
     </div>
-
-    <div v-if="filterStatus(2)" class="price-dialog-filter">
-        <div class="dialog-filter-head">
-
-            <div class="dialog-filter-head-column min">
-                precio min. {{min}}</div>
-            <div class="dialog-filter-head-column">precio max. {{max}}</div>
-
-        </div>
-        <div class="dialog-filter-head-column-options">
-
-            <div class="dialog-filter-head-option">
-                cualquier precio</div>
-            <div class="dialog-filter-head-option">cualquier precio</div>
-
-        </div>
-        <!-- ****v-for ↓*** -->
-        <div v-for="(range ,index) in minRanges" :key="index" class="dialog-filter-head-column-options">
-
-            <div class="dialog-filter-head-option"> {{ nFormatter(range,4)}}</div>
-
-        </div>
-
-        <div v-for="(range ,index) in maxRanges" :key="index" class="dialog-filter-head-column-options">
-
-<div class="dialog-filter-head-option">{{nFormatter(range,4)}}</div>
-
-</div>
-
-
-    </div>
-
-</div>
 </template>
 
 <script>
@@ -79,7 +84,7 @@ export default {
             toggleFilter,
             minRanges,
             maxRanges,
-            nFormatter:Utils.nFormatter
+            nFormatter: Utils.nFormatter
 
         }
     },
@@ -115,7 +120,7 @@ export default {
     padding: 10px;
     box-shadow: 0 2px 18px 0 rgb(0 0 0 / 10%), 0 3px 6px 0 rgb(0 0 0 / 20%);
     right: 0;
-    overflow: scroll;
+    overflow-y: scroll;
 }
 
 .dialog-filter-head {
@@ -161,6 +166,7 @@ export default {
 
 .dialog-filter-head-column-options {
     display: flex;
+    flex-wrap: wrap;
 }
 
 .dialog-filter-head-option {
@@ -184,6 +190,10 @@ export default {
 .price-filter {
     display: flex;
     flex-direction: column;
+}
+
+.price-filter:hover {
+    background: #e1e1e1;
 }
 
 .custom-filter {
